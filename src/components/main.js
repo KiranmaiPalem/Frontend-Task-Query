e5fb4e4f8e616961e552c07447b01f396972b003
Kiranmai Palem <palemkiranmai@gmail.com>
2019-11-19 22:24:27 +0530

    first commit

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Editor from './editor/Editor';
+import Notelist from './noteslist/Notelist';
+import Titlebar from './titlebar/Titlebar';
+
+function MainPage() {
+  return (
+    <React.Fragment>
+        
+    </React.Fragment>
+  );
+}
+
+export default MainPage;
